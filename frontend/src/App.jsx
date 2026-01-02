aa2a7d3af02cb8d2c019b786ff8e0f4d10346158
07alizaa <simkhadaaliza080@gmail.com>
2026-01-02 13:03:57 +0545

    Fixed errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ThemeProvider, useTheme } from './context/ThemeContext';
 import { ArtEngine } from './components/ArtEngine';
 import { Navbar } from './components/Navbar';
